f3078ac5834021203971655f6479b18069e69e9b
JoseCarlosMarRangel <1930082@upv.edu.mx>
2022-01-26 02:35:25 -0600

    base del programa terminada, falta C

--- operaciones.py
+++ operaciones.py
@@ -1,3 +1,6 @@
+from os import stat
+
+
 def mundo_basura():
     estado_meta = {'A': '0', 'B': '0', 'C': '0'}
     costo = 0
@@ -8,19 +11,121 @@ def acciones(estado_meta, costo):
     input_ubicacion = input("Ingrese la ubicacion: ")
     input_status = input(
         "Ingrese el estado del punto inicial: ")  # ! limpio o sucio (0,1)
-    input_suciolimpio = input("Ingrese el estado otra habitacion: ")
-    print("La ubicacion inicial es:" + str(estado_meta))
+    input_suciolimpio = input("Ingrese el estado de B: ")
+    print("La condicion inicial es:" + str(estado_meta))
 
     if input_ubicacion == 'A':
         print("La ubicacion es A")
         if input_status == '1':
             print("El punto inicial esta sucio")
-            if input_suciolimpio == '1':
-               print("La ubicacion A esta sucia")
-               estado_meta['A'] = '0'
-               costo += 1
-               print("El costo por limpiar a es: " + str(costo))
-               print("La ubicacion A esta limpia")
 
-               if input_ubicacion == '1':
-                   """"""
+            if input_suciolimpio == '1':
+                print("La ubicacion A esta sucia")
+                estado_meta['A'] = '0'
+                costo += 1
+                print("El costo por limpiar a es: " + str(costo))
+                print("La ubicacion A esta limpia")
+
+                if input_suciolimpio == '1':
+                    print("La ubicacion B esta sucia")
+                    print("Moviendo robot la ubicacion de B")
+                    costo += 1
+                    print("El costo por moverse es: " + str(costo))
+                    estado_meta['B'] = '0'
+                    costo += 1
+                    print("El costo por limpiar a es: " + str(costo))
+                    print("La ubicacion B esta limpia")
+
+                else:
+                    print("Sin accion" + str(costo))
+                    print("La  ubicacion B esta limpia")
+
+        if input_status == '0':
+            print("La ubicacion A esta limpia")
+            if input_suciolimpio == '1':
+                print("La ubicacion B esta sucia")
+                print("Moviendo robot la ubicacion de B")
+                costo += 1
+                print("El costo por moverse es: " + str(costo))
+                estado_meta['B'] = '0'
+                costo += 1
+                print("El costo por limpiar a es: " + str(costo))
+                print("La ubicacion B esta limpia")
+            else:
+                print("Sin accion" + str(costo))
+                print(costo)
+                print("La  ubicacion B esta limpia")
+
+    elif input_ubicacion == 'B':
+        print("La ubicacion es B")
+        if input_status == '1':
+            print("El punto inicial esta sucio")
+            estado_meta['B'] = '0'
+            costo += 1
+            print("El costo por limpiar a es: " + str(costo))
+            print("La ubicacion B esta limpia")
+
+            if input_suciolimpio == '1':
+                print("La ubicacion A esta sucia")
+                print("Moviendo robot la ubicacion de A")
+                costo += 1
+                print("El costo por moverse es: " + str(costo))
+                estado_meta['A'] = '0'
+                costo += 1
+                print("El costo por limpiar a es: " + str(costo))
+                print("La ubicacion A esta limpia")
+
+        else:
+            print("costo" + str(costo))
+            print("La ubicacion B esta limpia")
+            if input_suciolimpio == '1':
+                print("La ubicacion A esta sucia")
+                print("Moviendo robot la ubicacion de A")
+                costo += 1
+                print("El costo por moverse es: " + str(costo))
+                estado_meta['A'] = '0'
+                costo += 1
+                print("El costo por limpiar a es: " + str(costo))
+                print("La ubicacion A esta limpia")
+            else:
+                print("Sin accion" + str(costo))
+                print("La  ubicacion A esta limpia")
+
+    elif input_ubicacion == 'C':
+        print("La ubicacion es C")
+        if input_status == '1':
+            print("El punto inicial esta sucio")
+            estado_meta['C'] = '0'
+            costo += 1
+            print("El costo por limpiar a es: " + str(costo))
+            print("La ubicacion C esta limpia")
+
+            if input_suciolimpio == '1':
+                print("La ubicacion A esta sucia")
+                print("Moviendo robot la ubicacion de A")
+                costo += 1
+                print("El costo por moverse es: " + str(costo))
+                estado_meta['A'] = '0'
+                costo += 1
+                print("El costo por limpiar a es: " + str(costo))
+                print("La ubicacion A esta limpia")
+
+        else:
+            print("costo" + str(costo))
+            print("La ubicacion C esta limpia")
+            if input_suciolimpio == '1':
+                print("La ubicacion A esta sucia")
+                print("Moviendo robot la ubicacion de A")
+                costo += 1
+                print("El costo por moverse es: " + str(costo))
+                estado_meta['A'] = '0'
+                costo += 1
+                print("El costo por limpiar a es: " + str(costo))
+                print("La ubicacion A esta limpia")
+            else:
+                print("Sin accion" + str(costo))
+                print("La  ubicacion A esta limpia")
+
+    # Limpieza terminada
+    print("La condicion final es:" + str(estado_meta))
+    print("El preformance es: " + str(costo))
